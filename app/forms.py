1157e1c833c50215b6f1b43ac78772f243a77fe7
Lanai Nevers <lanainevers@gmail.com>
2020-03-14 23:55:36 -0500

    Added Form Classes - General & AboutYou

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+
+class General(FlaskForm):
+    fname = StringField('First Name:', validators=[InputRequired()])
+    lname = StringField('Last Name:', validators=[InputRequired()])
+    email = StringField('Email Address', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+
+class AboutYou(FlaskForm):
+    sex = SelectField('Sex', choices=[(0, 'Female'), (1, 'Male')])
+    age = SelectField(
+        'Age', choices=[(19, '19'), (20, '20'), (21, '21'), (22, '22')])
+    height = StringField('Height:', validators=[InputRequired()])
+    ethnicity = SelectField('What is your ethnicity?', choices=[(
+        'Black', 'Black (Coloured)'), ('Chinese', 'Chinese'), ('White', 'White'), ('Indian', 'Indian')])
+    personality = SelectField('What is your personality type?', choices=[(
+        'Introvert', 'Introvert'), ('Extrovert', 'Extrovert'), ('Ambivert', 'Ambivert')])
+
+    work = SelectField('To which work area do you belong?', choices=[
+        ('Business', 'Business'), ('Education', 'Education'), ('Science', 'Science'), ('Technology', 'Technology'), ('Construction', 'Construction'), ('Communication', 'Communication'), ('Law', 'Law')])
+
+    leadership = SelectField('How would you describe your leadership style?', choices=[(
+        'Democratic', 'Democratic'), ('Autocratic', 'Autocratic'), ('Laissez-Faire', 'Laissez-Faire')])
+
+    education = SelectField('Which is your level of education?', choices=[('Bachelors', 'Bachelors'), (
+        'Masters', 'Masters'), ('PhD', 'PhD'), ('Diploma', 'Diploma'), ('Associate Degree', 'Associate Degree')])
--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ from app import app
 from app import db, login_manager
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
-from app.forms import LoginForm
+from app.forms import LoginForm, General, AboutYou
 from app.models import User
 
 
@@ -78,14 +78,52 @@ def register():
     return render_template('register.html', form=form)
 
 
-@app.route('/registerRegular/')
+@app.route('/registerRegular/', methods=["GET", "POST"])
 def registerRegular():
-    return render_template('general_register.html')
+    form = General()
+    if request.method == "POST":
+        # change this to actually validate the entire form submission
+        # and not just one field
+        if form.username.data:
+            # Get the username and password values from the form.
+
+            # using your model, query database for a user based on the username
+            # and password submitted. Remember you need to compare the password hash.
+            # You will need to import the appropriate function to do so.
+            # Then store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method below.
+
+            # get user id, load into session
+            login_user(user)
+
+            # remember to flash a message to the user
+            # they should be redirected to a secure-page route instead
+            return redirect(url_for("home"))
+    return render_template("general_register.html", form=form)
 
 
-@app.route('/aboutRegular/')
+@app.route('/aboutRegular/', methods=["GET", "POST"])
 def aboutRegular():
-    return render_template('about_you.html')
+    form = AboutYou()
+    if request.method == "POST":
+        # change this to actually validate the entire form submission
+        # and not just one field
+        if form.username.data:
+            # Get the username and password values from the form.
+
+            # using your model, query database for a user based on the username
+            # and password submitted. Remember you need to compare the password hash.
+            # You will need to import the appropriate function to do so.
+            # Then store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method below.
+
+            # get user id, load into session
+            login_user(user)
+
+            # remember to flash a message to the user
+            # they should be redirected to a secure-page route instead
+            return redirect(url_for("home"))
+    return render_template('about_you.html', form=form)
 
 
 @app.route('/registerOrganizer/')
